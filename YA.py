638c42aa16d2fdf9ed7b2ede3245216924eadfc9
Bibek kun <bibekrai655@gmail.com>
2022-05-12 20:39:48 +0545

    trying pylint

--- YA.py
+++ YA.py
@@ -1,11 +1,6 @@
-a=[[1,2],
-  [3,4]]
-b=[[5,6],
-  [7,8]]
-result=[[0,0],
-       [0,0]]
-for i in range(len(a)):
-    for j in range(len(a[0])):
-        result[i][j]=a[i][j]*b[i][j]
-for r in result:
-    print(r)
+'''
+This program adds two numbers and displays their results
+'''
+A = 1
+B = 2
+print('Sum of Numbers:', A + B)
