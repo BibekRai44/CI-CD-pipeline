424f01e25c5ae18ea3d9598e819e7e4d7c217e8e
Bibek kun <bibekrai655@gmail.com>
2022-05-12 20:52:26 +0545

    trying pylint

--- YA.py
+++ YA.py
@@ -1,6 +1,6 @@
 '''
 This program adds two numbers and displays their results
 '''
-firstnum = 1
-secondnum = 2
-print('Sum of Numbers:', firstnum+ secondnum)
+Firstnum = 1
+Secondnum = 2
+print('Sum of Numbers:', Firstnum + Secondnum)
