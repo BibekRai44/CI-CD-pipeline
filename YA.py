a6604fc804e7a120657d7772bb20edc78039cfda
Bibek kun <bibekrai655@gmail.com>
2022-05-14 11:32:04 +0545

    running pylint

--- YA.py
+++ YA.py
@@ -1,8 +1,9 @@
-import os
-import math
+"""
+Code to add two numbers
+"""
 
-def func1(a, b) -> str:
-    return math.floor(a + b)
+a = 23
+b = 45
+c = a + b
 
-def func2(a,b, c) -> int:
-    return os.getcwd()
+print(c)
